9a143f977f97a7ab7a9a70dce40da34fc894bf63
Euro Dario Fuenmayor Contreras <development@eurofuenmayor.es>
2026-09-06 14:39:24 +0200

    Place profile portrait beside mobile metadata

--- resources/js/pages/ProfilePage.vue
+++ resources/js/pages/ProfilePage.vue
@@ -60,6 +60,7 @@ async function submitContact() {
           <div class="hero-meta">
             <span><v-icon icon="mdi-map-marker-outline" />{{ t.based }}</span>
             <span><v-icon icon="mdi-circle-medium" color="success" />{{ t.available }}</span>
+            <img class="mobile-portrait" :src="'/eurox.jpg'" alt="Euro Dario Fuenmayor Contreras" width="76" height="76">
           </div>
         </div>
         <div class="portrait-wrap">
